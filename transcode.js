236f2e8851d1022a933db7322040978cb9fbc026
Maxime Alardo <maxime.current@gmail.com>
2017-03-08 12:06:23 +0100

    add several qualities

--- transcode.js
+++ transcode.js
@@ -1,26 +1,27 @@
-
 "use strict"
 
-var util = require('util')
-var exec = require('child_process').exec;
-var fs = require('fs');
+let util = require('util')
+let exec = require('child_process').exec;
+let fs = require('fs');
 let request = require('request');
 
 
-var addr = "http://192.168.0.25:8080/video";
-var quality = "1000k";
+let addr = "http://192.168.1.137:8080/video";
+let quality = ["500k","1000k","2000k"];
 
-setTimeout(function() {
-                var child;
-        // executes `pwd`     
-        child = exec(`ffmpeg -use_wallclock_as_timestamps 1 -i ` + addr + ` -c copy -flags +global_header -map 0 -codec:v libx264 -profile:v main -b:v ` + quality + ` -vf scale=1920:1080 -x264opts keyint=12:min-keyint=12:scenecut=-1 -bf 0 -r 24 -f segment -segment_time 6 -segment_format_options movflags=+faststart -reset_timestamps 1 test%d.mp4`, function (error, stdout, stderr) {
-            util.print('stdout: ' + stdout);
-            util.print('stderr: ' + stderr);
-            if (error !== null) {
-                console.log('exec error: ' + error);
-            }
-        }); 
-}, 0);
+for (let i = 0; i < quality.length; i++) {
+    setTimeout(function() {
+                    var child;
+            // executes `pwd`
+            child = exec(`ffmpeg -use_wallclock_as_timestamps 1 -i ` + addr + ` -c copy -flags +global_header -map 0 -codec:v libx264 -profile:v main -b:v ` + quality[i] + ` -vf scale=1920:1080 -x264opts keyint=12:min-keyint=12:scenecut=-1 -bf 0 -r 24 -f segment -segment_time 6 -segment_format_options movflags=+faststart -reset_timestamps 1 ${quality[i]}/test%d.mp4`, function (error, stdout, stderr) {
+                util.print('stdout: ' + stdout);
+                util.print('stderr: ' + stderr);
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                }
+            });
+    }, 0);
+}
 
 //-use_wallclock_as_timestamps 1 pour forcer la lecture a la bonne vitesse
 //video.pipe(tmp);
