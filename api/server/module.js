6057f282ba280a8e9bd25cfb72b5f776334b2093
Lwiip <louisgalava@gmail.com>
2017-03-17 19:44:38 +0100

    hack done + refactory done

--- api/server/module.js
+++ api/server/module.js
@@ -4,6 +4,7 @@
 
 let express = require("express");
 let bodyParser = require("body-parser");
+let Infiniteloop = require("infinite-loop");
 
 module.exports = options => {
   let service = options.service;
@@ -11,16 +12,27 @@ module.exports = options => {
 
   let transition = {};
 
-  router.get("/localvideo", function(req, res) {    //route POST /video done before
+  router.get("/localvideo", function(req, res) { //small hack : route POST /video done before
     transition.pass.pipe(res);
   });
 
   router.post("/video", function(req, res) {
     transition.pass = req;
-  });
+    options.ffmpeg();
 
-    router.get("/test", function(req, res) {
-    res.send("test ok");
+    //loop for mp4tom4s
+    let loop1 = new Infiniteloop();
+    //use loop.add to add a function
+    //fisrt argument should be the fn, the rest is the fn's arguments
+    loop1.add(options.toolbox.mp4box, []);
+    loop1.setInterval(2000);
+    loop1.run();
+
+    //loop for renameSendDelete
+    let loop2 = new Infiniteloop();
+    loop2.add(options.toolbox.renameSendDelete, []);
+    loop2.setInterval(2000);
+    loop2.run();
   });
 
   return router;
