ce6d937ca3f4e5abfff56bd6bad5528f5fab66eb
SaadSerhani <saad.serhani7@gmail.com>
2017-03-24 17:02:54 +0100

    first version multiple live handling

--- execTranscoder.js
+++ execTranscoder.js
@@ -1,23 +1,21 @@
-"use strict"
-
-//var child_process = require('child_process');
-let contentId = ["123"]; //Id uploader
-let users = ["http://192.168.2.118:8080/video"]; //@ uploader
+let contentId = ['123','456']; //Id uploader
+let users = ['http://192.168.2.118:8080/video', 'http://192.168.2.119:8080/video']; //@ uploader
+let fs = require('fs');
 const spawn = require('child_process').spawn;
-
 let uploaderId = ['1', '2'], 
     quality = ['500k', '1000k', '2000k'];
 
 for(let i = 0; i < uploaderId.length; i++){
 if (!fs.existsSync(uploaderId[i])){ fs.mkdirSync(uploaderId[i]); }
-if (!fs.existsSync(uploaderId[i] + "/" + quality[0])){ fs.mkdirSync(uploaderId[0] +"/"+ quality[0]); }
-if (!fs.existsSync(uploaderId[i] + "/" + quality[1])){ fs.mkdirSync(uploaderId[0] +"/"+ quality[1]); }
-if (!fs.existsSync(uploaderId[i] + "/" + quality[2])){ fs.mkdirSync(uploaderId[0] +"/"+ quality[2]); }
+if (!fs.existsSync(uploaderId[i] + "/" + quality[0])){ fs.mkdirSync(uploaderId[i] +"/"+ quality[0]); }
+if (!fs.existsSync(uploaderId[i] + "/" + quality[1])){ fs.mkdirSync(uploaderId[i] +"/"+ quality[1]); }
+if (!fs.existsSync(uploaderId[i] + "/" + quality[2])){ fs.mkdirSync(uploaderId[i] +"/"+ quality[2]); }
 }
 
-for(let k = 0; k < contentId.length; k++){
-	const ls = spawn('node', ['transcode.js', contentId[k], users[k]]);
-
+let ls;
+//for(let k = 0; k < contentId.length; k++){
+	ls = spawn('node', ['transcode.js', contentId, users]);
+console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"+ " " + users.slice(1));
 	ls.stdout.on('data', (data) => {
 	  console.log(`stdout: ${data}`);
 	});
@@ -29,7 +27,7 @@ for(let k = 0; k < contentId.length; k++){
 	ls.on('close', (code) => {
 	  console.log(`child process exited with code ${code}`);
 	});
-}
+//}
 
 
 // exec: spawns a shell.
