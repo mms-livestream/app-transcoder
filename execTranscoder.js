7b73e744e135c53a1b7509ceae97ef05e7f1186c
Lwiip <louisgalava@gmail.com>
2017-03-24 13:49:15 +0100

    first version multiple live handling

--- execTranscoder.js
+++ execTranscoder.js
@@ -5,6 +5,16 @@ let contentId = ["123"]; //Id uploader
 let users = ["http://192.168.2.118:8080/video"]; //@ uploader
 const spawn = require('child_process').spawn;
 
+let uploaderId = ['1', '2'], 
+    quality = ['500k', '1000k', '2000k'];
+
+for(let i = 0; i < uploaderId.length; i++){
+if (!fs.existsSync(uploaderId[i])){ fs.mkdirSync(uploaderId[i]); }
+if (!fs.existsSync(uploaderId[i] + "/" + quality[0])){ fs.mkdirSync(uploaderId[0] +"/"+ quality[0]); }
+if (!fs.existsSync(uploaderId[i] + "/" + quality[1])){ fs.mkdirSync(uploaderId[0] +"/"+ quality[1]); }
+if (!fs.existsSync(uploaderId[i] + "/" + quality[2])){ fs.mkdirSync(uploaderId[0] +"/"+ quality[2]); }
+}
+
 for(let k = 0; k < contentId.length; k++){
 	const ls = spawn('node', ['transcode.js', contentId[k], users[k]]);
 
