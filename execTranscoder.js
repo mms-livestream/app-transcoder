f8e615c616d64ece9f47a5b326a56d2d9076d618
SaadSerhani <saad.serhani7@gmail.com>
2017-03-24 12:17:29 +0100

    [Transcoder] Multiple id/users handling

--- execTranscoder.js
+++ execTranscoder.js
@@ -1,18 +1,26 @@
 "use strict"
 
-var child_process = require('child_process');
-
+//var child_process = require('child_process');
 let contentId = ["123"]; //Id uploader
 let users = ["http://192.168.2.145:9000"]; //@ uploader
+const spawn = require('child_process').spawn;
 
-for(let i = 0; i < contentId.length; i++){
-//execFile: executes transcoder.js with the specified arguments. Changes in transcode.js : 
-//contendId => process.argv[2] 
-//destAddr => process.argv[3]
-child_process.execFile('node', ['transcoder.js', contentId[i], users[i]], function(error, stdout, stderr){
-	console.log(stdout);
-});
+for(let k = 0; k < contentId.length; k++){
+	const ls = spawn('node', ['transcode.js', contentId[k], users[k]]);
+
+	ls.stdout.on('data', (data) => {
+	  console.log(`stdout: ${data}`);
+	});
+
+	ls.stderr.on('data', (data) => {
+	  console.log(`stderr: ${data}`);
+	});
+
+	ls.on('close', (code) => {
+	  console.log(`child process exited with code ${code}`);
+	});
 }
+
 
 // exec: spawns a shell.
 /*child_process.exec('ls -l', function(error, stdout, stderr){
