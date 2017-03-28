4eb5868bfde237f21556707303ac485afd8f9b0b
Lwiip <louisgalava@gmail.com>
2017-03-28 13:48:06 +0200

    multiple live handling ok

--- execTranscoder.js
+++ execTranscoder.js
@@ -1,22 +1,21 @@
 "use strict";
 
-let contentId = ['1','2']; //Id uploader supprimer uploaderId qui sert a rien
-let users = ['http://192.168.2.118:8080/video', 'http://192.168.2.134:8080/video']; //@ uploader
+let contentId = ['1', '2']; 
+let users = ['http://192.168.2.118:8080/video', 'http://192.168.2.118:8080/video']; //@ content
 let fs = require('fs');
 const spawn = require('child_process').spawn;
-let uploaderId = ['1', '2'], 
-    quality = ['500k', '1000k', '2000k'];
+let quality = ['500k', '1000k', '2000k'];
 
-for(let i = 0; i < uploaderId.length; i++){
-if (!fs.existsSync(uploaderId[i])){ fs.mkdirSync(uploaderId[i]); }
-if (!fs.existsSync(uploaderId[i] + "/" + quality[0])){ fs.mkdirSync(uploaderId[i] +"/"+ quality[0]); }
-if (!fs.existsSync(uploaderId[i] + "/" + quality[1])){ fs.mkdirSync(uploaderId[i] +"/"+ quality[1]); }
-if (!fs.existsSync(uploaderId[i] + "/" + quality[2])){ fs.mkdirSync(uploaderId[i] +"/"+ quality[2]); }
+for(let i = 0; i < contentId.length; i++){
+if (!fs.existsSync(contentId[i])){ fs.mkdirSync(contentId[i]); }
+if (!fs.existsSync(contentId[i] + "/" + quality[0])){ fs.mkdirSync(contentId[i] +"/"+ quality[0]); }
+if (!fs.existsSync(contentId[i] + "/" + quality[1])){ fs.mkdirSync(contentId[i] +"/"+ quality[1]); }
+if (!fs.existsSync(contentId[i] + "/" + quality[2])){ fs.mkdirSync(contentId[i] +"/"+ quality[2]); }
 }
 
 let ls;
-//for(let k = 0; k < contentId.length; k++){
-	ls = spawn('node', ['transcodeMulti.js', contentId, users]);
+for(let k = 0; k < contentId.length; k++){
+	ls = spawn('node', ['transcodeMulti.js', contentId[k], users[k]]);
 console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"+ " " + users.slice(1));
 	ls.stdout.on('data', (data) => {
 	  console.log(`stdout: ${data}`);
@@ -29,10 +28,5 @@ console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"+ " " + users.slic
 	ls.on('close', (code) => {
 	  console.log(`child process exited with code ${code}`);
 	});
-//}
+}
 
-
-// exec: spawns a shell.
-/*child_process.exec('ls -l', function(error, stdout, stderr){
-	console.log(stdout);
-});*/
--- transcodeMulti.js
+++ transcodeMulti.js
@@ -14,17 +14,17 @@ let counter = 1; //global variable
 let counterFind = 0;
 let contentId = process.argv[2].split(",");
 
-for (let j = 0; j < contentId.length; j++) {
+
   for (let i = 0; i < quality.length; i++) {
     setTimeout(
       function() {
         var child;
         child = exec(
           `ffmpeg -use_wallclock_as_timestamps 1 -i ` +
-            addr[j] +
+            addr +
             ` -c copy -flags +global_header -map 0 -codec:v libx264 -profile:v main -b:v ` +
             quality[i] +
-            ` -vf scale=640:480 -x264opts keyint=12:min-keyint=12:scenecut=-1 -bf 0 -r 24 -f segment -segment_time 6 -segment_format_options movflags=+faststart -reset_timestamps 1 ${contentId[j]}/${quality[i]}/${quality[i]}_%d.mp4`,
+            ` -vf scale=640:480 -x264opts keyint=12:min-keyint=12:scenecut=-1 -bf 0 -r 24 -f segment -segment_time 6 -segment_format_options movflags=+faststart -reset_timestamps 1 ${contentId}/${quality[i]}/${quality[i]}_%d.mp4`,
           function(error, stdout, stderr) {
             util.print("stdout: " + stdout);
             util.print("stderr: " + stderr);
@@ -35,20 +35,18 @@ for (let j = 0; j < contentId.length; j++) {
         );
       },0);
   };
-};
 
 function mp4box() {
     console.log("=============================>>>>>>>>>>>>>>"+ counter);
-  for (let k = 0; k < contentId.length; k++){
   if (
-    fs.existsSync(contentId[k] + "/" + quality[0] + "/" + quality[0] + "_" + counter + ".mp4") &&
-    fs.existsSync(contentId[k] + "/" + quality[1] + "/" + quality[1] + "_" + counter + ".mp4") &&
-    fs.existsSync(contentId[k] + "/" + quality[2] + "/" + quality[2] + "_" + counter + ".mp4")
+    fs.existsSync(contentId + "/" + quality[0] + "/" + quality[0] + "_" + counter + ".mp4") &&
+    fs.existsSync(contentId + "/" + quality[1] + "/" + quality[1] + "_" + counter + ".mp4") &&
+    fs.existsSync(contentId + "/" + quality[2] + "/" + quality[2] + "_" + counter + ".mp4")
   ) {
     var counterTmp = counter - 1;
     var child;
     child = exec(
-      `MP4Box -dash 6000-profile live -bs-switching no -segment-name '${contentId[k]}/$RepresentationID$_${counterTmp}' -out 'mpd.mpd' ${contentId[k]}/${quality[0]}/${quality[0]}_${counterTmp}.mp4 ${contentId[k]}/${quality[1]}/${quality[1]}_${counterTmp}.mp4 ${contentId[k]}/${quality[2]}/${quality[2]}_${counterTmp}.mp4`,
+      `MP4Box -dash 6000-profile live -bs-switching no -segment-name '${contentId}/$RepresentationID$_${counterTmp}' -out 'mpd.mpd' ${contentId}/${quality[0]}/${quality[0]}_${counterTmp}.mp4 ${contentId}/${quality[1]}/${quality[1]}_${counterTmp}.mp4 ${contentId}/${quality[2]}/${quality[2]}_${counterTmp}.mp4`,
       function(error, stdout, stderr) {
         util.print("stdout: " + stdout);
         util.print("stderr: " + stderr);
@@ -59,67 +57,66 @@ function mp4box() {
     );
      counter++;
   }
-}}
+}
 
 
 function renameSendDelete () {
-  for (let k = 0; k < contentId.length; k++){
   if (
-    fs.existsSync(contentId[k] + "/" + "2_"+counterFind+"1.m4s") &&
-    fs.existsSync(contentId[k] + "/" + "3_"+counterFind+"1.m4s") &&
-    fs.existsSync(contentId[k] + "/" + "4_"+counterFind+"1.m4s")
+    fs.existsSync(contentId + "/" + "2_"+counterFind+"1.m4s") &&
+    fs.existsSync(contentId + "/" + "3_"+counterFind+"1.m4s") &&
+    fs.existsSync(contentId + "/" + "4_"+counterFind+"1.m4s")
   ) {
     var counterRename = counterFind + 1;
 
     /*----------------------------------- Quality 500k ---------------------------------*/
 
     // rename
-    fs.rename(contentId[k] + "/" + "2_"+counterFind+"1.m4s", contentId[k] + "/" + "out"+qualityDash[0]+"_dash"+counterRename+".m4s", function(err) {
+    fs.rename(contentId + "/" + "2_"+counterFind+"1.m4s", contentId + "/" + "out"+qualityDash[0]+"_dash"+counterRename+".m4s", function(err) {
       if ( err ) console.log('ERROR: ' + err);
       // Send
-      fs.createReadStream(contentId[k] + "/" + "out"+qualityDash[0]+"_dash"+counterRename+".m4s").pipe(request.put(destAddr +"/api/content/"+contentId[k]+"/"+qualityDash[0]+"/"+ "out"+qualityDash[0]+"_dash"+counterRename+".m4s"));
+      fs.createReadStream(contentId + "/" + "out"+qualityDash[0]+"_dash"+counterRename+".m4s").pipe(request.put(destAddr +"/api/content/"+contentId+"/"+qualityDash[0]+"/"+ "out"+qualityDash[0]+"_dash"+counterRename+".m4s"));
     });
     //Delete
-    fs.unlinkSync(contentId[k] + "/" +quality[0] + "/" +quality[0] + "_" + counterFind + ".mp4");
+    fs.unlinkSync(contentId + "/" +quality[0] + "/" +quality[0] + "_" + counterFind + ".mp4");
 
 
     /*------------------------------------- Quality 1000k -------------------------------*/
 
-  fs.rename(contentId[k] + "/" + "3_"+counterFind+"1.m4s", contentId[k] + "/" + "out"+qualityDash[1]+"_dash"+counterRename+".m4s", function(err) {
+  fs.rename(contentId + "/" + "3_"+counterFind+"1.m4s", contentId + "/" + "out"+qualityDash[1]+"_dash"+counterRename+".m4s", function(err) {
       if ( err ) console.log('ERROR: ' + err);
       // Send
-      fs.createReadStream(contentId[k] + "/" + "out"+qualityDash[1]+"_dash"+counterRename+".m4s").pipe(request.put(destAddr +"/api/content/"+contentId[k]+"/"+qualityDash[1]+"/"+ "out"+qualityDash[1]+"_dash"+counterRename+".m4s"));
+      fs.createReadStream(contentId + "/" + "out"+qualityDash[1]+"_dash"+counterRename+".m4s").pipe(request.put(destAddr +"/api/content/"+contentId+"/"+qualityDash[1]+"/"+ "out"+qualityDash[1]+"_dash"+counterRename+".m4s"));
     });
 
     //Delete
-    fs.unlinkSync(contentId[k] + "/" + quality[1] + "/" +quality[1] + "_" + counterFind + ".mp4");
+    fs.unlinkSync(contentId + "/" + quality[1] + "/" +quality[1] + "_" + counterFind + ".mp4");
   
     /*--------------------------------------- Quality 2000k --------------------------------*/
 
-  fs.rename(contentId[k] + "/" + "4_"+counterFind+"1.m4s", contentId[k] + "/" + "out"+qualityDash[2]+"_dash"+counterRename+".m4s", function(err) {
+  fs.rename(contentId + "/" + "4_"+counterFind+"1.m4s", contentId + "/" + "out"+qualityDash[2]+"_dash"+counterRename+".m4s", function(err) {
       if ( err ) console.log('ERROR: ' + err);
       // Send
-      fs.createReadStream(contentId[k] + "/" + "out"+qualityDash[2]+"_dash"+counterRename+".m4s").pipe(request.put(destAddr +"/api/content/"+contentId[k]+"/"+qualityDash[2]+"/"+ "out"+qualityDash[2]+"_dash"+counterRename+".m4s"));
+      fs.createReadStream(contentId + "/" + "out"+qualityDash[2]+"_dash"+counterRename+".m4s").pipe(request.put(destAddr +"/api/content/"+contentId+"/"+qualityDash[2]+"/"+ "out"+qualityDash[2]+"_dash"+counterRename+".m4s"));
     });
     //Delete
-    fs.unlinkSync(contentId[k] + "/" + quality[2] + "/" +quality[2] + "_" + counterFind + ".mp4");
+    fs.unlinkSync(contentId + "/" + quality[2] + "/" +quality[2] + "_" + counterFind + ".mp4");
 
     /*--------------------------------------Send mp4 ----------------------------------------*/
 
- fs.rename(contentId[k] + "/" + "2_0.mp4", contentId[k] + "/" + "out"+qualityDash[0]+"_dash"+".mp4", function(err) {
-      fs.createReadStream(contentId[k] + "/" + "out"+qualityDash[0]+"_dash"+".mp4").pipe(request.put(destAddr +"/api/mp4/" + contentId[k] + "/"+qualityDash[0]+"/"+ "out"+qualityDash[0]+"_dash"+".mp4"));
+ fs.rename(contentId + "/" + "2_0.mp4", contentId + "/" + "out"+qualityDash[0]+"_dash"+".mp4", function(err) {
+      fs.createReadStream(contentId + "/" + "out"+qualityDash[0]+"_dash"+".mp4").pipe(request.put(destAddr +"/api/mp4/" + contentId + "/"+qualityDash[0]+"/"+ "out"+qualityDash[0]+"_dash"+".mp4"));
     });
 
-    fs.rename(contentId[k] + "/" + "3_0.mp4", contentId[k] + "/" + "out"+qualityDash[1]+"_dash"+".mp4", function(err) {
-      fs.createReadStream(contentId[k] + "/" + "out"+qualityDash[1]+"_dash"+".mp4").pipe(request.put(destAddr +"/api/mp4/" + contentId[k] + "/"+qualityDash[1]+"/"+ "out"+qualityDash[1]+"_dash"+".mp4"));
+    fs.rename(contentId + "/" + "3_0.mp4", contentId + "/" + "out"+qualityDash[1]+"_dash"+".mp4", function(err) {
+      fs.createReadStream(contentId + "/" + "out"+qualityDash[1]+"_dash"+".mp4").pipe(request.put(destAddr +"/api/mp4/" + contentId + "/"+qualityDash[1]+"/"+ "out"+qualityDash[1]+"_dash"+".mp4"));
     });
 
-    fs.rename(contentId[k] + "/" + "4_0.mp4", contentId[k] + "/" + "out"+qualityDash[2]+"_dash"+".mp4", function(err) {
-      fs.createReadStream(contentId[k] + "/" + "out"+qualityDash[2]+"_dash"+".mp4").pipe(request.put(destAddr +"/api/mp4/" + contentId[k] + "/"+qualityDash[2]+"/"+ "out"+qualityDash[2]+"_dash"+".mp4"));
+    fs.rename(contentId + "/" + "4_0.mp4", contentId + "/" + "out"+qualityDash[2]+"_dash"+".mp4", function(err) {
+      fs.createReadStream(contentId + "/" + "out"+qualityDash[2]+"_dash"+".mp4").pipe(request.put(destAddr +"/api/mp4/" + contentId + "/"+qualityDash[2]+"/"+ "out"+qualityDash[2]+"_dash"+".mp4"));
     });
     counterFind ++;
   }
-}}
+}
 
 //loop for mp4tom4s
 var loop1 = new Infiniteloop();
